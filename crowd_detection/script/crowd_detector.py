bed205cf53ab6f1db026cdf90506bee5a3ee707a
quan <anhquan18>
2021-12-13 11:54:52 +0900

    add alert sound

--- crowd_detection/script/crowd_detector.py
+++ crowd_detection/script/crowd_detector.py
@@ -12,6 +12,9 @@ from cv_bridge import CvBridge
 import copy
 import cv2
 import time
+import numpy as np
+#from playsound import playsound
+from pygame import mixer
 
 
 class CrowdDetector(object):
@@ -20,31 +23,35 @@ class CrowdDetector(object):
         self.human_detector_sub = rospy.Subscriber("/darknet_ros/bounding_boxes", BoundingBoxes, self.human_detector_callback)
         self.raw_img_sub = rospy.Subscriber("/usb_cam/image_raw", Image, self.img_receive_callback, queue_size = 1)
         self.bridge = CvBridge()
+        self.sailen = "/home/quan/catkin_ws/src/detect_dense_crowd/crowd_detection/script/yuriko_test/20210922_024553.mp3"
+        self.mitsu = "/home/quan/catkin_ws/src/detect_dense_crowd/crowd_detection/script/yuriko_test/20210922_023949.mp3"
+        mixer.init()
+        mixer.music.load(self.sailen)
 
     def img_receive_callback(self, img):
         self.img = self.bridge.imgmsg_to_cv2(img, desired_encoding="bgr8")
 
     def human_detector_callback(self, detection_datas):
-        detected_human = {}
         human_data = []
 
         for obj in detection_datas.bounding_boxes:
-            if obj.Class == "person" and obj.probability >= 0.8:
+            if obj.Class == "person" and obj.probability >= 0.7:
                 person_bounding_x = (obj.xmax - obj.xmin) # image pixel col position
                 person_bounding_y = (obj.ymax - obj.ymin) # image pixel row position
 
-                detected_human["position"] = (person_bounding_x/2, person_bounding_y/2)
-                detected_human["size"] = (person_bounding_x, person_bounding_y)
+                detected_human = {}
+                #detected_human["position"] = (person_bounding_x/2, person_bounding_y/2)
+                #detected_human["size"] = (person_bounding_x, person_bounding_y)
                 detected_human["box"] = (obj.xmin, obj.xmax, obj.ymin, obj.ymax)
 
-                human_data.append(detected_human)
+                human_data.append(copy.deepcopy(detected_human))
 
         self.crowd_data = []
         self.dense_crowd_detector(human_data)
         self.visualize_crowd()
 
     def dense_crowd_detector(self, human_data):
-        crowd_thres = 30
+        overlap_thresh = 10
 
         while human_data:
             check_human = human_data.pop(0)
@@ -52,11 +59,14 @@ class CrowdDetector(object):
                 overlap_x = self.get_overlap(check_human["box"][:2], human["box"][:2])
                 overlap_y = self.get_overlap(check_human["box"][2:], human["box"][2:])
                 
-                if overlap_x >= crowd_thres and overlap_y >= crowd_thres:
-                    self.crowd_data.append([copy.deepcopy(check_human), copy.deepcopy(human)])
+                if overlap_x >= overlap_thresh and overlap_y >= overlap_thresh:
+                    group = [copy.deepcopy(check_human), copy.deepcopy(human)]
+                    if group not in self.crowd_data:
+                        self.crowd_data.append(group)
+            #print("crowd:", len(self.crowd_data))
+            #print self.crowd_data
+            #print
 
-        print("crowd:", len(self.crowd_data))
-        print self.crowd_data
 
     def get_overlap(self, a, b):
         return max(0, min(a[1], b[1]) - max(a[0], b[0]))
@@ -66,14 +76,15 @@ class CrowdDetector(object):
         color = (0,0,255)
         thickness = 2
 
-        img = copy.deepcopy(self.img)
-        
-        for crowd in self.crowd_data:
-            img = cv2.rectangle(img, (crowd[0]["box"][0], crowd[0]["box"][2]), (crowd[0]["box"][1], crowd[0]["box"][3]), color, thickness)
-            img = cv2.rectangle(img, (crowd[1]["box"][0], crowd[1]["box"][2]), (crowd[1]["box"][1], crowd[1]["box"][3]), color, thickness)
+        draw_img = copy.deepcopy(self.img)
 
+        for person1, person2 in self.crowd_data:
+            cv2.rectangle(draw_img, (person1["box"][0], person1["box"][2]), (person1["box"][1], person1["box"][3]), color, thickness)
+            cv2.rectangle(draw_img, (person2["box"][0], person2["box"][2]), (person2["box"][1], person2["box"][3]), color, thickness)
 
-        cv2.imshow(window_name, img)
+        if self.crowd_data and not mixer.music.get_busy():
+            mixer.music.play()
+        cv2.imshow(window_name, draw_img)
         cv2.waitKey(1)
 
 
